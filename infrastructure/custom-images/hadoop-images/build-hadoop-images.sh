3405013c24d8284cb3bc715cb719fec39f626374
Amin Borjian <borjianamin98@outlook.com>
2022-04-29 00:07:19 +0430

    Improve infrastructure configurations
      + Set timezone of all images
      + Set locale of all images

--- infrastructure/custom-images/hadoop-images/build-hadoop-images.sh
+++ infrastructure/custom-images/hadoop-images/build-hadoop-images.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
-
 cd "${SCRIPT_DIR}" || exit 1
+
 docker build --tag hadoop-base --file base/Dockerfile base
 docker build --tag hadoop-namenode --file namenode/Dockerfile namenode
 docker build --tag hadoop-datanode --file datanode/Dockerfile datanode
-docker build --tag hadoop-resourcemanager --file resourcemanager/Dockerfile resourcemanager
-docker build --tag hadoop-nodemanager --file nodemanager/Dockerfile nodemanager
-docker build --tag hadoop-historyserver --file historyserver/Dockerfile historyserver
-docker build --tag hadoop-wordcount --file wordcount/Dockerfile wordcount
+#docker build --tag hadoop-resourcemanager --file resourcemanager/Dockerfile resourcemanager
+#docker build --tag hadoop-nodemanager --file nodemanager/Dockerfile nodemanager
+#docker build --tag hadoop-historyserver --file historyserver/Dockerfile historyserver
+#docker build --tag hadoop-wordcount --file wordcount/Dockerfile wordcount
